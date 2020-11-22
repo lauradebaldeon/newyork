df261b302afa502ee61085b22d18223eb76f600d
Glitch (new-york-map) <none>
2020-11-22 16:33:27 +0000

    🐞🌯 Updated with Glitch

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) 2020 Laura de Baldeon
+Copyright (c) 2020 ldiezdeb
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
---
+++ activatefullpage.js
@@ -0,0 +1,84 @@
+// new fullpage('#fullpage', {
+//     //Navigation
+// 	licenseKey: 'YOUR KEY HERE',
+// 	interlockedSlidesKey:'YOUR KEY HERE',
+//     menu: '#menu',
+// 	lockAnchors: false,
+// 	anchors:['sec01', 'sec02','sec03','sec04', 'sec05','sec06','sec07', 'sec08','sec09','sec010', 'sec011','sec012','sec013','sec014','sec015'],
+// 	navigation: false,
+// 	navigationPosition: 'right',
+// 	navigationTooltips: ['firstSlide', 'secondSlide'],
+// 	showActiveTooltip: false,
+// 	slidesNavigation: false,
+
+// 	//Scrolling
+// 	css3: true,
+// 	scrollingSpeed: 700,
+// 	autoScrolling: true,
+// 	fitToSection: true,
+// 	fitToSectionDelay: 1000,
+// 	scrollBar: false,
+// 	easing: 'easeInOutCubic',
+// 	easingcss3: 'ease',
+// 	loopBottom: false,
+// 	loopTop: false,
+// 	loopHorizontal: false,
+// 	continuousVertical: false,
+// 	continuousHorizontal: false,
+// 	scrollHorizontally: false,
+// 	interlockedSlides: true,
+// 	dragAndMove: false,
+// 	offsetSections: false,
+// 	resetSliders: false,
+// 	fadingEffect: false,
+// 	normalScrollElements: '#element1, .element2',
+// 	scrollOverflow: false,
+// 	scrollOverflowReset: false,
+// 	scrollOverflowOptions: null,
+// 	touchSensitivity: 15,
+// 	bigSectionsDestination: null,
+
+// 	//Accessibility
+// 	keyboardScrolling: true,
+// 	animateAnchor: true,
+// 	recordHistory: true,
+
+// 	//Design
+// 	controlArrows: true,
+// 	verticalCentered: false,
+// 	responsiveWidth: 0,
+// 	responsiveHeight: 0,
+// 	responsiveSlides: false,
+// 	parallax: false,
+// 	cards: false,
+
+// 	//Custom selectors
+// 	sectionSelector: '.section',
+// 	slideSelector: '.slide',
+
+// 	lazyLoading: true,
+
+// 	//events
+// 	onLeave: function(origin, destination, direction){},
+// 	afterLoad: function(origin, destination, direction){},
+// 	afterRender: function(){},
+// 	afterResize: function(width, height){},
+// 	afterReBuild: function(){},
+// 	afterResponsive: function(isResponsive){},
+// 	afterSlideLoad: function(section, origin, destination, direction){},
+// 	onSlideLeave: function(section, origin, destination, direction){}
+// });
+
+
+// new fullpage('#fullpage', {
+// 	anchors:['sec01', 'sec02','sec03','sec04', 'sec05','sec06','sec07', 'sec08','sec09','sec010', 'sec011','sec012','sec013','sec014','sec015'],
+// 	menu: '#menu',
+// 	slidesNavigation: true,
+	
+// 	//In addition to the extension license you'll 
+// 	//need to acquire a fullPage.js license from https://goo.gl/5x9a22
+// 	licenseKey: 'INSERT YOUR FULLPAGE LICENSE KEY HERE',
+
+// 	interlockedSlides: true,
+// 	interlockedSlidesKey: 'INSERT YOUR EXTENSION KEY HERE' //see https://goo.gl/xkUmHS
+// });
